59a8906b6574c56541e23e3e8374ab96dd99cf53
macdre <ross.e.macd@gmail.com>
2020-04-24 20:01:51 -0300

    Fixed build problems with vue.config.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import { router } from './js/helpers';
 import { store } from './js/store';
-import App from './js/App';
+import App from './js/App.vue';
 
 // setup fake backend
 import { configureFakeBackend } from './js/helpers';
 configureFakeBackend();
 
 new Vue({
+  App,
   router,
   store,
   render: h => h(App)
--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,11 @@
-var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// Pull in webpack for style template
 module.exports = {
     configureWebpack: {
         mode: 'development',
-        resolve: {
-            extensions: ['.js', '.vue'],
-            alias: {
-                'vue$': 'vue/dist/vue.common.js'
-            }
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.vue?$/,
-                    exclude: /(node_modules)/,
-                    use: 'vue-loader'
-                },
-                {
-                    test: /\.js?$/,
-                    exclude: /(node_modules)/,
-                    use: 'babel-loader'
-                }
-            ]
-        },
-        plugins: [new HtmlWebpackPlugin({
-            template: './src/js/index.html'
-        })],
-        devServer: {
-            historyApiFallback: true
-        },
+        plugins: [
+            new HtmlWebpackPlugin({template: './src/js/index.html'})
+        ],
         externals: {
             // global app config object
             config: JSON.stringify({
