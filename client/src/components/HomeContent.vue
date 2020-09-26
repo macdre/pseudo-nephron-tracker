54338727cbee55fd4feb2cff3dfda160352db992
macdre <ross.e.macd@gmail.com>
2020-09-25 22:10:00 -0300

    Adding MenuCard capability

--- client/src/components/HomeContent.vue
+++ client/src/components/HomeContent.vue
@@ -3,77 +3,38 @@
     <vc-layout v-resize="resize" class="align-center mx-0" spacing="8">
       <vc-col :span="24">
         <vc-card class="elevation-3">
-
           <vc-card-title color="primary" :dark="false">
             <span class="body=2 vc-block">Home</span>
           </vc-card-title>
-
           <vc-card-text>
-            <vc-layout v-resize="resize" :class="['align-center', 'mx-0']" :spacing="8">
-
-              <vc-col :span="6" xs24 sm12 md6>
-                <vc-card class="elevation-6" tag="router-link" href="/treatment" style="text-decoration: none;">        
-                  <vc-card-media class="pa-0" height="25px"/>
-                  <vc-card-media class="pa-0" height="120px">
-                    <vc-icon class="fas" color="primary" size="x-large" style="width: 120px; height: 120px;">fa-notes-medical</vc-icon>   
-                  </vc-card-media>
-                  <vc-card-media class="pa-0" height="25px"/>
-                  <vc-card-title color="primary" :dark="false">
-                    <span class="body=2 vc-block">Record Treatment</span>
-                  </vc-card-title>
-                </vc-card>
-              </vc-col>
-
-              <vc-col :span="6" xs24 sm12 md6>
-                <vc-card class="elevation-6" tag="router-link" href="/inventory" style="text-decoration: none;">        
-                  <vc-card-media class="pa-0" height="25px"/>
-                  <vc-card-media class="pa-0" height="120px">
-                    <vc-icon class="fas" color="primary" size="x-large" style="width: 120px; height: 120px;">fa-boxes</vc-icon>   
-                  </vc-card-media>
-                  <vc-card-media class="pa-0" height="25px"/>
-                  <vc-card-title color="primary" :dark="false">
-                    <span class="body=2 vc-block">Manage Inventory</span>
-                  </vc-card-title>
-                </vc-card>
-              </vc-col>
-
-              <vc-col :span="6" xs24 sm12 md6>
-                <vc-card class="elevation-6" tag="router-link" href="/metrics" style="text-decoration: none;">        
-                  <vc-card-media class="pa-0" height="25px"/>
-                  <vc-card-media class="pa-0" height="120px">
-                    <vc-icon class="fas" color="primary" size="x-large" style="width: 120px; height: 120px;">fa-chart-bar</vc-icon>   
-                  </vc-card-media>
-                  <vc-card-media class="pa-0" height="25px"/>
-                  <vc-card-title color="primary" :dark="false">
-                    <span class="body=2 vc-block">Explore Metrics</span>
-                  </vc-card-title>
-                </vc-card>
-              </vc-col>
-
-              <vc-col :span="6" xs24 sm12 md6>
-                <vc-card class="elevation-6" tag="router-link" href="/setup" style="text-decoration: none;">        
-                  <vc-card-media class="pa-0" height="25px"/>
-                  <vc-card-media class="pa-0" height="120px">
-                    <vc-icon class="fas" color="primary" size="x-large" style="width: 120px; height: 120px;">fa-cogs</vc-icon>   
-                  </vc-card-media>
-                  <vc-card-media class="pa-0" height="25px"/>
-                  <vc-card-title color="primary" :dark="false">
-                    <span class="body=2 vc-block">Configure Templates</span>
-                  </vc-card-title>
-                </vc-card>
-              </vc-col>
- 
+            <vc-layout v-resize="resize" :class="['align-center', 'mx-0']">
+              <menu-card v-for="card in cards" v-bind:key="card.id" v-bind:card="card"/>      
             </vc-layout>
           </vc-card-text>
         </vc-card>
       </vc-col>
     </vc-layout>
   </b-container>
-
 </template>
 
 <script>
+
+import MenuCard from "../components/MenuCard";
+
 export default {
-  name: "HomeContent"
+  name: "HomeContent",
+  components: {
+    MenuCard
+  },
+  data() {
+    return {
+      cards: [
+        { id: 1, routerLink: "/treatment", iconName: "fa-notes-medical", cardTitle: "Record Treatment" },
+        { id: 2, routerLink: "/inventory", iconName: "fa-boxes", cardTitle: "Manage Inventory" },
+        { id: 3, routerLink: "/metrics", iconName: "fa-chart-bar", cardTitle: "Explore Metrics" },
+        { id: 4, routerLink: "/setup", iconName: "fa-cogs", cardTitle: "Configure Templates" }
+      ]
+    }
+  }
 };
 </script>
---
+++ client/src/components/MenuCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <vc-col :span="6" style="padding: 8px;" xs24 sm12 md6>
+    <vc-card tag="router-link" :href="card.routerLink" style="text-decoration: none;"
+        :class="`${hover ? 'elevation-24': 'elevation-6'}`" @mouseover.native="hover = true" @mouseleave.native="hover = false">
+      <vc-card-media class="pa-0" height="25px"/>
+      <vc-card-media class="pa-0" height="120px">
+        <vc-icon class="fas" color="primary" size="x-large" style="width: 120px; height: 120px;">{{ card.iconName }}</vc-icon>
+      </vc-card-media>
+      <vc-card-media class="pa-0" height="25px"/>
+      <vc-card-title color="primary" :dark="false">
+        <span class="body=2 vc-block">{{ card.cardTitle }}</span>
+      </vc-card-title>
+    </vc-card>
+  </vc-col>
+</template>
+
+<script>
+  export default {
+    name: "MenuCard",
+    props: [
+      'card'
+    ],
+    data() {
+      return {
+        hover: false
+      }
+    }
+  };
+</script>
