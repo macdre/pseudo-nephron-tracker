a23096ab710ffccee75b8f18739ec8ff9dd5b703
macdre <ross.e.macd@gmail.com>
2021-10-30 12:58:20 -0300

    adding error logs

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,8 +2,9 @@
 echo "Received ${PORT} as PORT for nginx server"
 sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf 
 
-touch /var/log/gunicorn.log
-gunicorn -b 0.0.0.0:5000 --chdir ./server app:app --log-file /var/log/gunicorn.log --daemon &
+touch /var/log/gunicorn.err.log
+touch /var/log/gunicorn.std.log
+gunicorn -b 0.0.0.0:5000 --chdir ./server app:app --error-logfile /var/log/gunicorn.err.log --log-file /var/log/gunicorn.std.log --daemon &
 nginx -g 'daemon off;' &
 
-tail -f /var/log/nginx/error.log -f /var/log/gunicorn.log
+tail -f /var/log/nginx/error.log -f /var/log/gunicorn.err.log -f /var/log/gunicorn.std.log
