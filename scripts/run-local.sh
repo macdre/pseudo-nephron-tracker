6e88c4e4cfe95f5b2af1579120d136638f26847a
macdre <ross.e.macd@gmail.com>
2021-12-06 19:31:27 -0400

    Fixing deps

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DATABASE_URL=postgresql://qvfbxecawwyrzq:fee378800db04735f3fbc7d5154fcec2f5d918068a3edf5d6796e6fed45378d8@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d5svua9jn755cj
+export DATABASE_URL=postgres://lmjttnprcsswre:3f25c5c085562657a6056c038c1461d4910161dafc15ef79b6e0de8091081888@ec2-3-224-112-203.compute-1.amazonaws.com:5432/d9r43ts7v3t6nh
 
 if [ -z ${1+x} ]; then
 	currDir=$(pwd)
